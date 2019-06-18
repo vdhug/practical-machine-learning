a644d5400d82e3121e5143bf2b5a85437bc48b9e
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-06-18 01:20:49 -0300

    Using regression to train and test

--- finance-regression.py
+++ finance-regression.py
@@ -1,7 +1,9 @@
 # Example Regression
 import pandas as pd
-import quandl
-import math
+import quandl, math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -31,3 +33,18 @@ forecast_out = int(math.ceil(0.01 * len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
 print(df.head())
+
+# Features
+X = np.array(df.drop(['label'], 1))
+# Labels
+y = np.array(df['label'])
+
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
