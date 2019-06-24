9618f54cefda4ac1a50e00f9cc335a664d0f4e17
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-06-24 15:51:31 -0300

    Finishing calculate the best fit slope and intercept, testing the prediction

--- manual-regression.py
+++ manual-regression.py
@@ -1,16 +1,32 @@
 from statistics import mean
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
 
+style.use('fivethirtyeight')
 
 # Definition of a simple straight line: y = mx + b
 xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
 ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
 
 # Function that define the best fit slope
-def best_fit_slope(xs, ys):
+def best_fit_slope_and_intercept(xs, ys):
     m = (((mean(xs)*mean(ys)) - mean(xs*ys)) /
         ((mean(xs)**2) - mean(xs**2)))
-    return m
+    b = mean(ys) - (m * mean(xs))
+    return m, b
 
 
-print(best_fit_slope(xs, ys))
+
+m, b = best_fit_slope_and_intercept(xs, ys)
+
+# Draw regression line
+regression_line = [ (m*x) + b for x in xs]
+
+# Testing prediction
+predict_x = 8
+predict_y = (m*predict_x) + b
+plt.scatter(xs, ys)
+plt.scatter(predict_x, predict_y)
+plt.plot(xs, regression_line)
+plt.show()
