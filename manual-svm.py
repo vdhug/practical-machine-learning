d7ff389252375baf918f07e7a882c2a7691580ec
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-06-27 15:06:19 -0300

    Finishing optimization of SVM algorithm

--- manual-svm.py
+++ manual-svm.py
@@ -34,6 +34,8 @@ class Support_Vector_Machine:
         # no need to keep this memory.
         all_data=None
 
+        # Support Vectors is equal to yi(xi.w+b) = 1
+        # You will know when you get a good result when in boht your positive and negative classes you have a value close to one. How close? This is up to you, remembering that SVM is a problem of optimization, so if you want to be really precise, you will have to pay the price of speed
         step_sizes = [self.max_feature_value * 0.1,
                       self.max_feature_value * 0.01,
                       # starts getting very high cost after this.
@@ -47,16 +49,50 @@ class Support_Vector_Machine:
             # we can do this because convex
             optimized = False
             while not optimized:
-                pass
+                for b in np.arange(-1*(self.max_feature_value*b_range_multiple),
+                                   self.max_feature_value*b_range_multiple,
+                                   step*b_multiple):
+                    for transformation in transforms:
+                        w_t = w*transformation
+                        found_option = True
+                        # weakest link in the SVM fundamentally
+                        # SMO attempts to fix this a bit
+                        # yi(xi.w+b) >= 1
+                        # 
+                        # #### add a break here later..
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi=i
+                                if not yi*(np.dot(w_t,xi)+b) >= 1:
+                                    found_option = False
+                                    
+                        if found_option:
+                            opt_dict[np.linalg.norm(w_t)] = [w_t,b]
 
+                if w[0] < 0:
+                    optimized = True
+                    print('Optimized a step.')
+                else:
+                    # Example: w = [5, 5]
+                    # step = 1
+                    # w - step ====> [5, 5] - [1, 1]
+                    w = w - step
+
+            # Sorting the array of optimals magnitudes
+            norms = sorted([n for n in opt_dict])
+            #||w|| : [w,b]
+            opt_choice = opt_dict[norms[0]]
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+            latest_optimum = opt_choice[0][0]+step*2
 
 
     def predict(self,features):
         # sign( x(i).w+b )
         classification = np.sign(np.dot(np.array(features),self.w)+self.b)
-
         return classification
         
+
 data_dict = {-1:np.array([[1,7],
                           [2,8],
                           [3,8],]),
