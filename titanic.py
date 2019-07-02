13d89526537e065a309c27b6d5e037c9e6e37091
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-07-02 17:10:10 -0300

    Applying K-means algorithm to the titanic dataset

--- titanic.py
+++ titanic.py
@@ -56,4 +56,23 @@ def handle_non_numerical_data(df):
     return df
 
 df = handle_non_numerical_data(df)
+
+# df.drop(['sex','boat'], 1, inplace=True)
 print(df.head())
+
+X = np.array(df.drop(['survived'], 1).astype(float))
+X = preprocessing.scale(X)
+y = np.array(df['survived'])
+
+clf = KMeans(n_clusters=2)
+clf.fit(X)
+
+correct = 0
+for i in range(len(X)):
+    predict_me = np.array(X[i].astype(float))
+    predict_me = predict_me.reshape(-1, len(predict_me))
+    prediction = clf.predict(predict_me)
+    if prediction[0] == y[i]:
+        correct += 1
+
+print(correct/len(X))
