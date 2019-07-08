c4b359935360be2550f9e3961c0e0d7cb9b899e0
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-07-08 20:46:40 -0300

    Tensorflow basics

---
+++ deep-learning/tensorflow-basic.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+# creates nodes in a graph
+# "construction phase"
+x1 = tf.constant(5)
+x2 = tf.constant(6)
+
+result = tf.mul(x1,x2)
+print(result)
+
+# defines our session and launches graph
+sess = tf.Session()
+# runs result
+print(sess.run(result))
+
+output = sess.run(result)
+print(output)
+
+sess.close()
+
+# with tf.Session() as sess:
+#     output = sess.run(result)
+#     print(output)
